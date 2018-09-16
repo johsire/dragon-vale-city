f5b8ef30044b280c3c048660c32aacd55dbdaea1
Josiah Maina <josiah5115@gmail.com>
2018-09-16 15:42:09 -0600

    db n schemas for the traits

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -9,5 +9,7 @@ createdb -U node_user dragonstackdb
 
 psql -U node_user dragonstackdb < ./bin/sql/generation.sql
 psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
+psql -U node_user dragonstackdb < ./bin/sql/trait.sql
+
 
 echo "dragonstackdb Configured!"
