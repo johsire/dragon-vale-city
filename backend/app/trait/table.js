7afd124f47fc8a7edbc8b361fb01d25ce6bbeebf
Josiah Maina <josiah5115@gmail.com>
2018-09-16 16:55:11 -0600

    fixed pool import require syntax err

--- backend/app/trait/table.js
+++ backend/app/trait/table.js
@@ -1,24 +1,25 @@
 
-const pool = ('../../databasePool.js');
+const pool = require('../../databasePool.js');
+
 
 class TraitTable {
-  static getTraitId({ traitType, traitValue }) {
-    return new Promise((resolve, reject) => {
-      pool.query(
-        'SELECT id FROM trait WHERE "traitType" = $1 AND "traitValue" = $2',
-        [traitType, traitValue],
-        (error, response) => {
-          if (error) return reject(error);
+ static getTraitId({ traitType, traitValue }) {
+   return new Promise((resolve, reject) => {
+     pool.query(
+       'SELECT id FROM trait WHERE "traitType" = $1 AND "traitValue" = $2',
+       [traitType, traitValue],
+       (error, response) => {
+         if (error) return reject(error);
 
-          resolve({ traitId: response.rows[0].id }) 
-        }
-      )
-    })
-  }
-};
+         resolve({ traitId: response.rows[0].id });
+       }
+     )
+   });
+ }
+}
 
 TraitTable.getTraitId({ traitType: 'backgroundColor', traitValue: 'green' })
    .then(({ traitId }) => console.log('traitId', traitId))
    .catch(error => console.log('error', error));
 
-module.exports = TraitTable;
+   module.exports = TraitTable;
