ecd4769637b5c9ccdfdd49e0f8c16956684ffad2
Josiah Maina <josiah5115@gmail.com>
2018-09-20 14:41:22 -0600

    session comp part 1

---
+++ backend/app/account/session.js
@@ -0,0 +1,28 @@
+
+const uuid = require('uuid/v4');
+const { hash } = require('./helper');
+
+class Session {
+    constructor({ username }) {
+        this.username = username;
+        this.id = uuid();
+    };
+
+    toStrinf() {
+      const { username, id } = this;
+
+      return Session.sessionString({ username, id });
+    };
+
+    static accountData({ username, id }) {
+      return `${username}|${id}`;
+    };
+
+    static sessionString({ username, id }) {
+      const accountData = Session.accountData({ username, id });
+
+      return `${accountData}|${hash(accountData)}`
+    }
+};
+
+module.exports = Session;
