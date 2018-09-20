b6a67a346d32d711a164e3846241538aa99bf919
Josiah Maina <josiah5115@gmail.com>
2018-09-20 17:41:00 -0600

    sessions

--- backend/app/account/helper.js
+++ backend/app/account/helper.js
@@ -3,7 +3,7 @@ const SHA256 = require('crypto-js/sha256');
 const { APP_SECRETE } = require('../../secrets');
 
 
-const hash = string => { 
+const hash = string => {
  return SHA256(`${ APP_SECRETE }${ string }${ APP_SECRETE }`).toString();
 };
 
--- backend/app/account/session.js
+++ backend/app/account/session.js
@@ -2,27 +2,56 @@
 const uuid = require('uuid/v4');
 const { hash } = require('./helper');
 
+const SEPARATOR = '|'
+
 class Session {
     constructor({ username }) {
         this.username = username;
         this.id = uuid();
     };
 
-    toStrinf() {
+    toString() {
       const { username, id } = this;
 
       return Session.sessionString({ username, id });
     };
 
+    static parse(sessionString) {
+      const sessionData = sessionString.split(SEPARATOR);
+
+      return {
+        username: sessionData[0],
+        id: sessionData[1],
+        sessionHash: sessionData[2] 
+      }
+  };
+
+  // verify with sessions that user is registered n logedin:
+  static verify(sessionString) {
+    const { username, id, sessionHash } = Session.parse(sessionString);
+
+    const accountData = Session.accountData({ username, id });
+
+    return hash(accountData) === sessionHash;
+  };
+
     static accountData({ username, id }) {
-      return `${username}|${id}`;
+      return `${username}${SEPARATOR}${id}`;
     };
 
     static sessionString({ username, id }) {
       const accountData = Session.accountData({ username, id });
 
-      return `${accountData}|${hash(accountData)}`
+      return `${accountData}${SEPARATOR}${hash(accountData)}`
     }
 };
+
+const foo = new Session({ username: 'foo'});
+
+const fooString = foo.toString();
+
+console.log('Session.parse(fooString', Session.parse(fooString));
+
+console.log(Session.verify(fooString));
 
 module.exports = Session;
