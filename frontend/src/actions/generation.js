3e78c24d074e2028508819203535197c9f54992d
Josiah Maina <josiah5115@gmail.com>
2018-09-19 21:02:24 -0600

    updated fetch gen Action

--- frontend/src/actions/generation.js
+++ frontend/src/actions/generation.js
@@ -1,11 +1,35 @@
 
-import { GENERATION_ACTION_TYPE } from './types';
+import { GENERATION } from './types';
 
 // Action Creator is wrapped around the ACTION -
 // its returning it in itself BUT its not the action creator itself.
-export const generationActionCreator = (payload) => {
-    return {
-      type: GENERATION_ACTION_TYPE,
-      generation: payload
-    }
-  };
+// export const generationActionCreator = (payload) => {
+//     return {
+//       type: GENERATION_ACTION_TYPE,
+//       generation: payload
+//     }
+//   };
+
+export const fetchGeneration = () => dispatch => {
+  dispatch({ type: GENERATION.FETCH });
+
+  return fetch('http://localhost:3000/generation')
+    .then(response => response.json())
+    .then(json => {
+      if(json.type === 'error') {
+        dispatch({
+          type: GENERATION.FETCH_ERROR,
+          message: json.message
+        });
+      } else {
+      dispatch({ 
+        type: GENERATION.FETCH_SUCCESS,
+        generation: json.generation
+        });
+      }  
+    })
+    .catch(error => dispatch({ 
+      type: GENERATION.FETCH_ERROR,
+      message: error.message
+   }));
+};
--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -60,15 +60,6 @@ const mapStateToProps = state => {
 //   }
 // };
 
-const fetchGeneration = () => dispatch => {
-  return fetch('http://localhost:3000/generation')
-    .then(response => response.json())
-    .then(json => {
-      dispatch(generationActionCreator(json.generation))
-    })
-    .catch(error => console.error('error<==xx', error));
-};
-
 const componentConnector = connect(
   mapStateToProps, 
   { fetchGeneration }
