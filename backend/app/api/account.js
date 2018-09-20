2bc75955d8474281165ee871c383dc276e872bce
Josiah Maina <josiah5115@gmail.com>
2018-09-20 14:10:29 -0600

    store&getAccount fnctns

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -10,6 +10,19 @@ router.post('/signup', (req, res, next) => {
  const usernameHash = hash(username);
  const passwordHash = hash(password);
 
+ AccountTable.getAccount({ usernameHash })
+  .then(({ account }) => {
+    if (!account) {
+      AccountTable.storeAccount({ usernameHash, passwordHash })
+        .then(() => res.json({ message: 'Success!!' }))
+        .catch(error => next(error));
+    } else {
+      const error = new Error('This username is already taken');
+
+      error.statusCode = 409;
+    };
+  });
+
   AccountTable.storeAccount({ usernameHash, passwordHash })
     .then(() => res.json({ message: 'success!' }))
     .catch(error => next(error));
