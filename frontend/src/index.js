2eaa190afb70d9bbf84de78f252b98d9c3b7a207
Josiah Maina <josiah5115@gmail.com>
2018-09-19 18:46:03 -0600

    redux - Actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,30 @@ import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 import './index.css';
 
+
 const DEFAULT_GENERATION = { generationId: '', expiration: '' }
 
-const generationReducer = () => {
+const generationReducer = (state, action) => {
+  console.log('generationReducer state', state);
+  console.log('generationReducer action', action);
+
+  if (action.type === 'GENERATION_ACTION_TYPE') {
+    return { generation: action.generation }
+  }
+  
   return { generation: DEFAULT_GENERATION }
 };
 
 const store = createStore(generationReducer);
-console.log('store', store);
+// console.log('store', store);
+// console.log('store.getState()', store.getState());
+
+store.dispatch({ type: 'foo'});
+store.dispatch({ 
+  type: 'GENERATION_ACTION_TYPE',
+  generation: { generationId: 'goo', expiration: 'bar'}
+});
+
 console.log('store.getState()', store.getState());
 
 render (
