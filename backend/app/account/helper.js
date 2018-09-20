df6947698570b7dc34f8c6cbf55c6618a0e97909
Josiah Maina <josiah5115@gmail.com>
2018-09-20 11:20:40 -0600

    HASH Secretes

---
+++ backend/app/account/helper.js
@@ -0,0 +1,10 @@
+
+const SHA256 = require('crypto-js/sha256');
+const { APP_SECRETE } = require('../../secrets');
+
+
+const hash = string => { 
+ return SHA256(`${ APP_SECRETE }${ string }${ APP_SECRETE }`).toString();
+};
+
+module.exports = { hash };
