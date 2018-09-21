6420678206cf14a57c70f99cf4a564a3632894b1
Josiah Maina <josiah5115@gmail.com>
2018-09-21 10:46:38 -0600

    logout init

--- frontend/src/reducers/account.js
+++ frontend/src/reducers/account.js
@@ -20,6 +20,14 @@ const account = (state = DEFAULT_ACCOUNT, action) => {
        loggedIn: true 
      };
 
+    case ACCOUNT.FETCH_LOGOUT_SUCCESS:
+     return { 
+      ...state, 
+      status: fetchStates.success,
+      message: action.message, 
+      loggedIn: false  
+     };
+
     default:
      return state;
   };
