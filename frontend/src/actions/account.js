71bb57a8cd4795a78a0ce181f9785174fc84995b
Josiah Maina <josiah5115@gmail.com>
2018-09-21 11:16:30 -0600

    login/signup refactored

--- frontend/src/actions/account.js
+++ frontend/src/actions/account.js
@@ -2,20 +2,16 @@
 import { ACCOUNT } from './types';
 import { BACKEND } from '../config';
 
-export const signup = ({ username, password }) => dispatch => {
+const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
   dispatch({ type: ACCOUNT.FETCH });
 
-  return fetch(`${BACKEND.ADDRESS}/account/signup`, {
-    method: 'POST',
-    body: JSON.stringify({ username, password }),
-    headers: { 'Content-Type': 'application/json' },
-    credentials: 'include'
-  }).then(response => response.json())
+  return fetch(`${BACKEND.ADDRESS}/account/${endpoint}`, options)
+    .then(response => response.json())
     .then(json => {
      if (json.type === 'error') {
       dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message })
      } else {
-      dispatch({ type: ACCOUNT.FETCH_SUCCESS, ...json });
+      dispatch({ type: SUCCESS_TYPE, ...json });
      }
    })
     .catch(error => dispatch({
@@ -23,18 +19,19 @@ export const signup = ({ username, password }) => dispatch => {
    }));
 };
 
-export const logout = () => dispatch => {
-  dispatch({ type: ACCOUNT.FETCH });
-
-  return fetch(`${BACKEND.ADDRESS}/account/logout`, {
+export const signup = ({ username, password }) => fetchFromAccount({
+  endpoint: 'signup',
+  options: {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: { 'Content-Type': 'application/json' },
     credentials: 'include'
-  }).then(response => response.json())
-    .then(json => {
-      if (json.type === 'error') {
-        dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
-      } else {
-        dispatch({ type: ACCOUNT.FETCH_LOGOUT_SUCCESS, ...json });
-      }
-    })
-    .catch(error => dispatch({ type: ACCOUNT.FETCH_ERROR, message: error.message }));
-};
+  },
+  SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
+});
+
+export const logout = () => fetchFromAccount ({
+  endpoint: 'logout',
+  options: { credentials: include },
+  SUCCESS_TYPE: ACCOUNT.FETCH_LOGOUT_SUCCESS
+});
