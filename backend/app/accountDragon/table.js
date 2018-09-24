d236e64017c7a9c8b2666d83964510c90fcfaacc
Josiah Maina <josiah5115@gmail.com>
2018-09-24 13:42:59 -0600

    accDragn - table.js Get Acc Dragons

--- backend/app/accountDragon/table.js
+++ backend/app/accountDragon/table.js
@@ -14,12 +14,31 @@ class AccountDragonTable {
         }
       )
     });
-  };
+  }
+
+  static getAccountDragons({ accountId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'SELECT "dragonId" FROM accountDragon WHERE "accountId" = $1',
+        [accountId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          resolve({ accountDragons: response.rows });
+        }
+      )
+    })
+  }
 };
 
 // AccountDragonTable.storeAccountDragon({
 //   accountId: 1, dragonId: 3
 // }).then(() => console.log('stored account dragon'))
 //   .catch(error => console.error(error, 'error<<==xxx'));
+
+
+// AccountDragonTable.getAccountDragons({ accountId: 2 })
+//   .then(({ accountDragons }) => console.log('accountDragons', accountDragons))
+//   .catch(error => console.error(error, 'error <==xxxxx'))
 
 module.exports = AccountDragonTable;
