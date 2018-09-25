561e38d39702b3fae6a8303c946b5f5896c4666e
Josiah Maina <josiah5115@gmail.com>
2018-09-25 11:01:58 -0600

    Acc Bal & Info

--- backend/app/account/table.js
+++ backend/app/account/table.js
@@ -1,12 +1,14 @@
 
 const pool = require('../../databasePool');
+const { STARTING_BALANCE } = require('../config');
 
 class AccountTable {
   static storeAccount({ usernameHash, passwordHash }) {
     return new Promise((resolve, reject) => {
       pool.query(
-       'INSERT INTO account ("usernameHash", "passwordHash") VALUES($1, $2)',
-       [usernameHash, passwordHash],
+       `INSERT INTO account ("usernameHash", "passwordHash", balance)
+       VALUES($1, $2, $3)`,
+       [usernameHash, passwordHash, STARTING_BALANCE ],
        (error, response) => {
          if (error) return reject(error)
 
@@ -19,7 +21,7 @@ class AccountTable {
   static getAccount({ usernameHash }) {
     return new Promise((resolve, reject) => {
       pool.query(
-        `SELECT id, "passwordHash", "sessionId" 
+        `SELECT id, "passwordHash", "sessionId", balance 
          FROM account 
          WHERE "usernameHash" = $1`,
         [usernameHash],
--- backend/app/config.js
+++ backend/app/config.js
@@ -7,10 +7,13 @@ const DAYS = HOURS * 24;
 // units:
 const REFRESH_RATE = 5;
 
+const STARTING_BALANCE = 50;
+
 module.exports = {
  SECONDS,
  MINUTES,
  HOURS,
  DAYS,
- REFRESH_RATE
+ REFRESH_RATE,
+ STARTING_BALANCE
 };
