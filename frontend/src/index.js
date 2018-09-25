830c2ca510cda6ea6b46bec518845b183572eee3
Josiah Maina <josiah5115@gmail.com>
2018-09-25 07:37:01 -0600

    Auth Routes: Stateless Fntnl Comps

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
@@ -21,13 +21,11 @@ const store = createStore(
   applyMiddleware(thunk)
 );
 
-class RedirectToAccountDragons extends Component {
-  render() {
+const RedirectToAccountDragons = () => {
     return (
       <Redirect to={{ pathname: '/account-dragons' }} />
     )
-  }
-}
+  };
 
 store.dispatch(fetchAuthenticated())
   .then(() => {
