be2fb85980afd76376612350bc43bdafeeec9ab8
Josiah Maina <josiah5115@gmail.com>
2018-09-26 19:35:44 -0600

    Buy button n method n fetch method

--- frontend/src/components/PublicDragonRow.js
+++ frontend/src/components/PublicDragonRow.js
@@ -1,16 +1,35 @@
 // dev dependencies imports
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 // component imports
 import DragonAvatar from './DragonAvatar';
+import { BACKEND } from '../config';
 
 class PublicDragonRow extends Component {
+ buy = () => {
+  const { dragonId, saleValue } = this.props.dragon;
+
+  fetch(`${BACKEND.ADDRESS}/dragon/buy`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'CONTENT_TYPE': 'application/json' },
+    body: JSON.stringify({ dragonId, saleValue })
+  }).then(response => response.json())
+    .then(json => {
+     alert(json.message);
+    })
+    .catch(error => alert(error.message));
+ }
+
  render() {
   return (
    <div>
     <div>{this.props.dragon.nickname}</div>
     <DragonAvatar dragon={this.props.dragon} />
     <div>Sale Value: {this.props.dragon.saleValue}</div>
+    <br />
+    <Button onClick={this.buy}>Buy</Button>
    </div>
   )
  }
